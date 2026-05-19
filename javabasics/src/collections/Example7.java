928674fd9267202feac11430aa60392e90535676
tausif900 <ansaritausif9268@gmail.com>
2026-05-19 15:39:27 +0530

    Day-21

--- javabasics/src/collections/Example7.java
+++ javabasics/src/collections/Example7.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 public class Example7 {
 	public static void main(String[] args) {
 		Stack<Integer> stack = new Stack<Integer>();
+		System.out.println(stack.empty());
 		stack.push(12);
 		stack.push(30);
 		stack.push(120);
@@ -15,6 +16,17 @@ public class Example7 {
 
 		System.out.println("Popped Element is: " + stack.pop()); // this return which element is removed.
 		System.out.println(stack);
+
+		stack.push(90);
+		stack.push(101);
+		System.out.println(stack.peek()); // peek() just checks the last element without removing any element.
+		System.out.println(stack);
+
+		System.out.println(stack.empty()); // it returns true or false
+		System.out.println(stack.search(101)); // it returns index number but from ends element. if the element is not
+												// present which we r searching for it returns -1.
 	}
 }
 //	Stack-It follows LIFO principle 'Last in First Out'. Undo and Renu features can be implemented using stack only.
+
+// banace parathesis, infix,prefix,postfix.
---
+++ javabasics/src/collections/Example8.java
@@ -0,0 +1,12 @@
+package collections;
+
+public class Example8 {
+	public static void main(String[] args) {
+
+	}
+}
+
+//	Set - It has only unique values.It has three implementations HashSet,LinkedHashSet and TreeSet.All three has same features
+// HashSet doesn't has guarantee sequence.Hashing technique is used internally.
+//LinkedHashSet internally inherits HashSet only, but the structure that they are using is LinkedList. Data is ordered in LinkedHashSet.
+//TreeSet - Values are unique. Things are sorted already.Integers sorted in ascending.Product,student then we have to decide.
