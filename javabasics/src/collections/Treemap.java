86e03292e4c7a094f3ed3037be0b907cc76d9e8c
tausif900 <ansaritausif9268@gmail.com>
2026-05-25 22:00:10 +0530

    Day-23

--- javabasics/src/collections/Treemap.java
+++ javabasics/src/collections/Treemap.java
@@ -7,10 +7,22 @@ import java.util.TreeMap;
 public class Treemap {
 	public static void main(String[] args) {
 		TreeMap<Integer, List<String>> batches = new TreeMap<Integer, List<String>>();
-
-		batches.put(101, Arrays.asList("Tausif", "Saif", "Khatija", "Arman"));
+//	null key is  not allowed as sorting has to do here.
+		batches.put(101, Arrays.asList("Tausif", "Saif", "Khatija", "Arman", "Khizer"));
 		batches.put(102, Arrays.asList("Nisha", "Laik", "Zaid", "Arkum"));
 		batches.put(103, Arrays.asList("Anisha", "Manisha", "Bhatya", "Desai"));
-		System.out.println(batches);
+
+		for (Integer i : batches.keySet()) {
+			System.out.println("Batch code:" + i);
+//			int countStudents = 0;
+			List<String> students = batches.get(i);
+			for (String name : students) {
+				System.out.println(name);
+//				countStudents++;
+			}
+//			System.out.println("Number of Students in the batch are:" + countStudents);
+			System.out.println("Total Number of Students:" + students.size());
+			System.out.println("----------------");
+		}
 	}
 }
