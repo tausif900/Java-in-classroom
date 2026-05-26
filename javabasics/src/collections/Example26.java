8622a8c3856f38046a6eeb02edc3fb0e5b24c73e
tausif900 <ansaritausif9268@gmail.com>
2026-05-26 14:43:01 +0530

    day-23

--- javabasics/src/collections/Example26.java
+++ javabasics/src/collections/Example26.java
@@ -8,5 +8,9 @@ public class Example26 {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(12, 56, 90, 100);
 		Optional<Integer> first = list.stream().findFirst();
+		System.out.println(first.get() + 123);
+
+		System.out.println(Arrays.asList().stream().findFirst().get());
+//		If the list is empty then it gives exception"NoSuchElementException".
 	}
 }
