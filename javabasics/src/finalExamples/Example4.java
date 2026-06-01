ebcf88311572dd661a32fc99774a0f3e90d6e909
tausif900 <ansaritausif9268@gmail.com>
2026-06-01 09:04:28 +0530

    classroom

---
+++ javabasics/src/finalExamples/Example4.java
@@ -0,0 +1,10 @@
+package finalExamples;
+
+public class Example4 {
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		e1.setId(101);
+		e1.setName("Tausif");
+		e1.setStatus(Status.ACTIVE);
+	}
+}
