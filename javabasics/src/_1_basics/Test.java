6a50f0f1cad85e845703dd2b19ab321455bfc24b
tausif900 <ansaritausif9268@gmail.com>
2026-08-06 20:29:49 +0530

    test

---
+++ javabasics/src/_1_basics/Test.java
@@ -0,0 +1,20 @@
+package _1_basics;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+//		System.out.println("Enter a number");
+//
+//		int num = input.nextInt();
+
+		int arr[] = new int[6];
+
+		arr[7] = 2;
+
+	}
+
+}
