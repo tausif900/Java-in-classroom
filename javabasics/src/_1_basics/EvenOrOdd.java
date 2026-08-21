54176c5232ee7604f6e0e603df019a859045a4cf
tausif900 <ansaritausif9268@gmail.com>
2026-08-21 21:36:15 +0530

    practice 1

---
+++ javabasics/src/_1_basics/EvenOrOdd.java
@@ -0,0 +1,16 @@
+package _1_basics;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num = input.nextInt();
+		if (num % 2 == 0) {
+			System.out.println(num + " is an even number");
+		} else {
+			System.out.println(num + " is an odd number");
+		}
+	}
+}
